9c446fd5b41f40b04120d7ec8856dfd361b89331
dsaf2007 <dsaf2007@naver.com>
2021-04-04 02:15:59 +0900

    2021 04 04

--- Week5/src/Main.java
+++ Week5/src/Main.java
@@ -1,3 +1,4 @@
+//2016112154 정동구
 import java.util.Scanner;
 
 
@@ -8,22 +9,23 @@ public class Main {
         Scanner sc =new Scanner(System.in);
         Student student = new Student();
 
-        for(int i = 0 ; i < student.size ; i++)
+        for(int i = 0 ; i < student.size ; i++)//while문 for문으로 변경
         {
             System.out.print("이름을 입력해주세요 : ");
             student.u_name[i]=sc.nextLine();
         }
-        for(int i = 0 ; i < student.size ; i++)
+        for(int i = 0 ; i < student.size ; i++) // while 문 for문으로 변경
         {
-            System.out.print("점수를 입력해주세요 : ");
+            System.out.print(student.u_name[i]+"의 점수를 입력해주세요 : ");
             student.u_score[i]=sc.nextInt();
         }
         for(int i = 0 ; i < student.size ; i++)
         {
-            switch(student.u_score[i]/10)
+            switch(student.u_score[i]/10)//case를 성적/10으로 나눔.
             {
                 case 10:
                     System.out.println(student.u_name[i] + " grade is A");
+                    break;
                 case 9:
                     System.out.println(student.u_name[i] + " grade is A");
                     break;
